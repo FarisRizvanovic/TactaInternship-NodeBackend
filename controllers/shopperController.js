16cd4e777457b2f0a82aead52132eef07d0f4ef9
Faris Rizvanovic <faris.rizvanovic00@gmail.com>
2023-11-16 19:20:49 +0100

    Add routes for getting shoppers and their items by user ID

--- controllers/shopperController.js
+++ controllers/shopperController.js
@@ -1,5 +1,6 @@
 const Shopper = require('../models/shopperModel');
 const catchAsync = require('../utils/catchAsync');
+const Item = require('../models/itemModel');
 
 exports.createShopper = catchAsync(async (req, res, next) => {
   const newShopper = await Shopper.create({
@@ -23,6 +24,41 @@ exports.getAllShoppers = catchAsync(async (req, res, next) => {
     results: shoppers.length,
     data: {
       shoppers,
+    },
+  });
+});
+
+exports.getShoppersForUser = catchAsync(async (req, res, next) => {
+  const shoppers = await Shopper.find({ user: req.params.userId }).select(
+    '-__v',
+  );
+
+  res.status(200).json({
+    status: 'success',
+    results: shoppers.length,
+    data: {
+      shoppers,
+    },
+  });
+});
+
+exports.getShoppersWithItemsForUser = catchAsync(async (req, res, next) => {
+  const shoppers = await Shopper.find({ user: req.params.userId }).select(
+    '-__v',
+  );
+
+  const shoppersWithItems = await Promise.all(
+    shoppers.map(async (shopper) => {
+      const items = await Item.find({ shopper: shopper._id }).select('-__v');
+      return { ...shopper._doc, items };
+    }),
+  );
+
+  res.status(200).json({
+    status: 'success',
+    results: shoppersWithItems.length,
+    data: {
+      shoppersWithItems,
     },
   });
 });
--- routes/shopperRoutes.js
+++ routes/shopperRoutes.js
@@ -3,6 +3,13 @@ const shopperController = require('../controllers/shopperController');
 
 const router = express.Router();
 
+router.get('/user/:userId', shopperController.getShoppersForUser);
+
+router.get(
+  '/user/:userId/items',
+  shopperController.getShoppersWithItemsForUser,
+);
+
 router
   .route('/')
   .get(shopperController.getAllShoppers)
